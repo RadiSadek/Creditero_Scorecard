eaf5449a923cb00f41a16fa585d39fc7e9fd7484
RadiSadek <60088784+RadiSadek@users.noreply.github.com>
2024-05-13 09:19:37 +0300

    remove 50% of Inds new

--- Cutoffs.R
+++ Cutoffs.R
@@ -5,13 +5,10 @@
 ################################################################
 
 # First Application Creditero
-#cu_first_app_proposed <- c(0.35,0.31,0.28,0.24,0.19)
-cu_first_app <- c(0.35,0.306,0.284,0.266,0.245)
-#cu_first_app_target <- c(0.268,0.214,0.162,0.117,0.078)
+cu_first_app <- c(0.324,0.306,0.284,0.266,0.245)
 
 # Repeat Creditero
 cu_repeat_app <- c(0.42,0.34,0.27,0.22,0.15)
-#cu_repeat_app_old <- c(0.4,0.26,0.19,0.13,0.08)
 
 # Pre score
 cu_pre_score <- c(0.46,0.43,0.4,0.34)
